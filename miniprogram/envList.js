cd8af51f213a321d4bccf91a8839485f83250ba5
abulili <2094994033@qq.com>
2023-04-27 16:57:28 +0800

    html_write

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-2gawvzff9ce25847","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-2gawvzff9ce25847 --n quickstartFunctions --r --project "D:\weiXin\miniprogram-4" --report_first --report
